78377d636439e774961aeb9360607127c1be004f
sam065819 <xdliverblx2@gmail.com>
2024-09-12 16:18:03 +0200

    code 2

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import discord
+import os
 
-class main(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+from dotenv import load_dotenv
+from discord.ext import commands
 
-    @commands.command(
-        name="say",
-        description="say",
-        usage="say <message>",
-    )
-    async def say(self, *, message: str) -> None:
-        await ctx.channel.send(message)
+load_dotenv()
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='c!', intents=intents)
+
+async def on_ready(self):
+    print(f'Logged on as {self.user}!')
+
+@bot.command()
+async def say(ctx, arg):
+    await ctx.send(arg)
+    
+bot.run(os.getenv("TOKEN"))
